876103004cb43e54103b0bbe6b114ce13c0bc483
Nasan Jargal <nasanjargal20040524@gmail.com>
2024-12-14 22:21:41 +0800

    commit

--- eventlistener.js
+++ eventlistener.js
@@ -1,9 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById("my-button");
-
+//document.addEventListener("DOMContentLoaded", () => {
+/*    const button = document.getElementById("my-button");
+    const display = document.getElementById("display")
+    let counter = 0
     button.addEventListener("click", doSomething);
-
-    function doSomething() {
-        alert("hello world");
+    function doSomething () {
+        counter++
+        display.innerHTML = counter;
     }
-});
+//});
+*/
+
+//window.addEventListener('keydown', function(e) {
+//    console.log(e);
+//})
+/*const player = document.getElementById('player')
+
+window.addEventListener('keydown', function(e){
+    if(e.code == 'Space') {
+        player.jump()
+    }
+})
+    */
+
+window.addEventListener('keydown',function(e) {
+    console.log(e.code);
+})
