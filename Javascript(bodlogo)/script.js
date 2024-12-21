3819d1aab9cdec3a5cfc04ca6735ce15dac19155
Nasan Jargal <nasanjargal20040524@gmail.com>
2024-12-21 22:16:57 +0800

    commit

--- Javascript(bodlogo)/script.js
+++ Javascript(bodlogo)/script.js
@@ -1,5 +1,18 @@
+// 1. Return the Sum of Two Numbers
+// let a = -5; let b = 2
+// console.log(a - b);
 
-let a = 5; let b = 2
-console.log(a + b);
+// 2. Convert Minutes into Seconds
+// let minutes = prompt("minut oruulna uu")
+// let second = minutes * 60;
+
+// console.log(second);
+
+// function findMinutes (minute){
+//     let second = minute * 60;
+//     return second
+// }
+// let input = Number(prompt("minut oruulna uu"));
+// console.log(findMinutes(input));
 
 
