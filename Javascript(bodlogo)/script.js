de99c76a184523ca86cb16fb15cef13bc94dd4d5
De1oNn <nasanjargal20040524@gmail.com>
2024-12-23 10:01:52 +0800

    commit

--- Javascript(bodlogo)/script.js
+++ Javascript(bodlogo)/script.js
@@ -16,3 +16,83 @@
 // console.log(findMinutes(input));
 
 
+// 3. Return the Next Number from the Integer Passed
+
+// let a = 2;
+
+// console.log(a+1);
+
+// function numberafter(nice) {
+//     console.log(nice + 1);
+//     return
+// }
+// numberafter(10)
+
+// 4. Area of a Triangle
+
+// function triangle() {
+//     let a = prompt("ehnii too oruul")
+//     let b = prompt("dragin too oruul")
+//     console.log((a * b) / 2);
+//     return
+// }
+// triangle();
+// 5. Convert Age to Days
+
+// function days() {
+//     let a = prompt("nas aa oruulna uu")
+//     console.log(a * 365);
+//     return
+// }
+// days();
+
+// 6. Buggy Code (Part 1)
+
+// function third() {
+//     let a = prompt("too oruulna uu")
+//     b = Math.pow(a, 3)
+//     console.log(b);
+//     return
+// }
+// third();
+
+// 7. Return the First Element in an Array
+
+// Array = [1, 2, 3, 4]
+
+// function firstnumber() {
+//     b = Array[0];
+//     console.log(b);
+//     return
+// }
+// firstnumber();
+
+// 8. Power Calculator
+
+// function power() {
+//     a = prompt("ehnii too oruul")
+//     b = prompt("dragin too oruul")
+//     console.log(a * b);
+//     return
+// }
+// power();
+
+// 9. Convert Hours into Seconds
+
+// function second() {
+//     a = prompt("tsag oruulna uu")
+//     console.log(a * 3600);
+//     return 
+// }
+// second();
+
+// 10. Maximum Edge of a Triangle
+
+// function triangle(){
+//     a = prompt("gurvaljiin neg tal")
+//     b = prompt("nuguu tal")
+//     c = `${(a * b) - 1} "uldsen tal"`
+//     console.log(c);
+//     return
+// }
+// triangle();
